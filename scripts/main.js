0795310942f826332461903765b1ebd2e78e52cf
Emas <emas.dev@gmail.com>
2021-01-28 11:16:50 -0600

    Add numbers of books in info

--- scripts/main.js
+++ scripts/main.js
@@ -23,6 +23,9 @@ bookTable.classList.add(
 const info = document.createElement("aside");
 info.classList.add("bg-red-400", "border-l-2", "w-1/4", "pt-20");
 info.innerHTML = "<h3 class='text-center'>Information</h3>";
+info.innerHTML += "<hr>";
+info.innerHTML += '<div class="flex justify-between p-4"><p>Books</p><p>' + myLibrary.length + '</p></div>';
+info.innerHTML += '<div class="flex justify-between p-4"><p>Completed Books</p><p> - </p></div>';
 
 library.appendChild(bookTable);
 library.appendChild(info);
